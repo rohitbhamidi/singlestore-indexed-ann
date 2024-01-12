6b800aec33f7efcfc852450e3435042c43f591d3
rohit bhamidipati <rbhamidipati@singlestore.com>
2024-01-12 12:15:40 -0800

    +(wiki scraping | semantic search)

---
+++ s2_openai_info.py
@@ -0,0 +1,9 @@
+API_KEY = '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'
+EMBEDDING_MODEL = 'text-embedding-ada-002'
+GPT_MODEL = 'gpt-3.5-turbo'
+
+USERNAME = 'admin'
+PASSWORD = 'SingleStore1!-v2'
+CONN_STR = 'svc-4d9215ce-fc86-42cb-9bf7-c5e1383bb647-dml.aws-oregon-4.svc.singlestore.com'
+PORT = '3306'
+DATABASE = 'wikipedia_test'
